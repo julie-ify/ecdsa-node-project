351bf119ad6a1a2543f847012ba28208a82eca4c
Juliana Ifionu <julieifionu@gmail.com>
2024-09-06 07:25:44 -0400

    Enable transaction signature and pass it to the server

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,54 +1,66 @@
-import { useState } from "react";
-import server from "./server";
+import { useState } from 'react';
+import server from './server';
+import { secp256k1 } from 'ethereum-cryptography/secp256k1.js';
+import { bytesToHex as toHex } from 'ethereum-cryptography/utils.js';
+import { hashMessage } from './util';
 
-function Transfer({ address, setBalance }) {
-  const [sendAmount, setSendAmount] = useState("");
-  const [recipient, setRecipient] = useState("");
+function Transfer({ address, setBalance, privateKey }) {
+	const [sendAmount, setSendAmount] = useState('');
+	const [recipient, setRecipient] = useState('');
 
-  const setValue = (setter) => (evt) => setter(evt.target.value);
+	const setValue = (setter) => (evt) => setter(evt.target.value);
 
-  async function transfer(evt) {
-    evt.preventDefault();
+	async function transfer(evt) {
+		evt.preventDefault();
+		try {
+			const messageHash = hashMessage(sendAmount);
+			const signature = secp256k1.sign(messageHash, privateKey);
+			const r = signature.r.toString();
+			const s = signature.s.toString();
+			const publicKey = toHex(secp256k1.getPublicKey(privateKey));
 
-    try {
-      const {
-        data: { balance },
-      } = await server.post(`send`, {
-        sender: address,
-        amount: parseInt(sendAmount),
-        recipient,
-      });
-      setBalance(balance);
-    } catch (ex) {
-      alert(ex.response.data.message);
-    }
-  }
+			const {
+				data: { balance },
+			} = await server.post(`send`, {
+				sender: publicKey,
+				r,
+				s,
+				amount: parseInt(sendAmount),
+				recipient,
+			});
 
-  return (
-    <form className="container transfer" onSubmit={transfer}>
-      <h1>Send Transaction</h1>
+			setBalance(balance);
+		} catch (ex) {
+			console.error('Error:', ex.response ? ex.response.data.message : ex.message)
+			alert(`Error: ${ex.response ? ex.response.data.message : ex.message}`);
+		}
+	}
 
-      <label>
-        Send Amount
-        <input
-          placeholder="1, 2, 3..."
-          value={sendAmount}
-          onChange={setValue(setSendAmount)}
-        ></input>
-      </label>
+	return (
+		<form className="container transfer" onSubmit={transfer}>
+			<h1>Send Transaction</h1>
 
-      <label>
-        Recipient
-        <input
-          placeholder="Type an address, for example: 0x2"
-          value={recipient}
-          onChange={setValue(setRecipient)}
-        ></input>
-      </label>
+			<label>
+				Send Amount
+				<input
+					placeholder="1, 2, 3..."
+					value={sendAmount}
+					onChange={setValue(setSendAmount)}
+				></input>
+			</label>
 
-      <input type="submit" className="button" value="Transfer" />
-    </form>
-  );
+			<label>
+				Recipient
+				<input
+					placeholder="Type an address, for example: 0x2"
+					value={recipient}
+					onChange={setValue(setRecipient)}
+				></input>
+			</label>
+
+			<input type="submit" className="button" value="Transfer" />
+		</form>
+	);
 }
 
 export default Transfer;
