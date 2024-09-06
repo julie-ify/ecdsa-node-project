8b15e2f54602f7a3e690b06b9ea707965e6b2343
Juliana Ifionu <julieifionu@gmail.com>
2024-09-06 07:19:02 -0400

    Update wallet to receive pk

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,31 +1,49 @@
-import server from "./server";
+import server from './server';
+import { secp256k1 } from 'ethereum-cryptography/secp256k1.js';
+import { toHex } from 'ethereum-cryptography/utils.js';
 
-function Wallet({ address, setAddress, balance, setBalance }) {
-  async function onChange(evt) {
-    const address = evt.target.value;
-    setAddress(address);
-    if (address) {
-      const {
-        data: { balance },
-      } = await server.get(`balance/${address}`);
-      setBalance(balance);
-    } else {
-      setBalance(0);
-    }
-  }
+function Wallet({
+	address,
+	setAddress,
+	privateKey,
+	setPrivateKey,
+	balance,
+	setBalance,
+}) {
+	async function onChange(evt) {
+		const privateKey = evt.target.value;
+		setPrivateKey(privateKey);
 
-  return (
-    <div className="container wallet">
-      <h1>Your Wallet</h1>
+		// publickey is referred to as address
+		const address = toHex(secp256k1.getPublicKey(privateKey));
+		setAddress(address);
 
-      <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
-      </label>
+		if (address) {
+			const {
+				data: { balance },
+			} = await server.get(`balance/${address}`);
+			setBalance(balance);
+		} else {
+			setBalance(0);
+		}
+	}
 
-      <div className="balance">Balance: {balance}</div>
-    </div>
-  );
+	return (
+		<div className="container wallet">
+			<h1>Your Wallet</h1>
+
+			<label>
+				Private Key
+				<input
+					placeholder="Type a private key, for example: 0x1"
+					value={privateKey}
+					onChange={onChange}
+				></input>
+			</label>
+			<div>public key: {address.slice(0, 10)}...</div>
+			<div className="balance">Balance: {balance}</div>
+		</div>
+	);
 }
 
 export default Wallet;
